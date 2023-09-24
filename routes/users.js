b7755b8125b67ca2bc5bce4f6bb60aedfa76fbf8
suranimaria <surani_maria@yahoo.com>
2023-09-24 01:37:46 +0100

    Fix server port

--- routes/users.js
+++ routes/users.js
@@ -58,14 +58,13 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecret"),
-        {
-          expiresIn: 360000,
-        },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
         }
       );
+
+      console.log("Sign")
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
